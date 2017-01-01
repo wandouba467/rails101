d3259949e70d490c91e80dde42c033c298ccb9b0
王天禹 <wandouba467@gmail.com>
2017-01-01 22:41:06 +0800

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
-RRails.application.routes.draw do
-  root 'welcome#index'
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
